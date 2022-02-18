3721a41e1745446c794e02719fa9d47df350cdbc
Vladislav Tereshchenko <vladis@inbox.ru>
2022-02-19 03:27:03 +0700

    git config

--- tools/git-global-init.cmd
+++ tools/git-global-init.cmd
@@ -2,8 +2,8 @@
 
 @rem for current user
 
-git config --global user.name "Your Name"
-git config --global user.email your@email
+git config --global user.name "Vladislav Tereshchenko"
+git config --global user.email vladis@inbox.ru
 
 @rem global
 
